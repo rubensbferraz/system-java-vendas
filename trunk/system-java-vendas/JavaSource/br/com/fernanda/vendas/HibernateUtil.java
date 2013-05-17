ad224e99fcc7405c35e191fe7a31e141928984cb
fernandalima2005@gmail.com <fernandalima2005@gmail.com@49a8d890-4e18-7f70-e8ca-dfb1fafa863d>
2013-05-17 14:16:44 +0000

    Criação das configurações do hibernate.

---
+++ trunk/system-java-vendas/JavaSource/br/com/fernanda/vendas/HibernateUtil.java
@@ -0,0 +1,27 @@
+/*Aqui devolve a sessão pro usuário*/
+
+package br.com.fernanda.vendas;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	
+	private static final SessionFactory session = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory(); //retorna a sessão
+		} catch (Throwable e) {
+			System.out.println("Algum problema ocorreu!\n" + e);
+			throw new ExceptionInInitializerError();
+		}
+		
+	}
+
+	public static SessionFactory getSession() {
+		return session;
+	}
+}
